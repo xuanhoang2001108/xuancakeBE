6076ea1918b32225a249faa490210dc2f5959ef8
Xuan Hoang <xuanhoang2001108@gmail.com>
2023-09-25 01:40:42 +0700

    12

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,5 +1,6 @@
-
-require("dotenv").config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 async function connectToDb() {
